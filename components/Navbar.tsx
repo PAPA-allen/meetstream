a346272d5a5c40049855705695a83da65b59e9db
allenaryee <allen.nii@icloud.com>
2024-08-01 11:46:13 +0000

    name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const s = () => {
     <div className="flex justify-between z-50 shadow-sm fixed h-[80px] w-full bg-background px-6 items-center">
       <Link href="/" className="flex gap-1 items-center">
       <Image src="icons/Logo.svg" alt="logo" width={40} height={40} className="justify-center flex items-center"/>
-      <p className="font-extrabold text-2xl">Meet<span className="text-blue-500">Stream</span></p>
+      <p className="font-extrabold text-2xl">Meet<span className="text-blue-500">Streams</span></p>
       </Link>
       <div className="flex gap-4">
     <SignedIn>
